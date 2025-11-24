9846fe219c80f2c60ac0d2637722c5829dbfa7c2
Manhattan Calabro <manhattan.n.calabro@gmail.com>
2025-11-23 17:15:08 -0800

    Environmental scroll

--- Assets/Scripts/EnvironmentScroll.cs
+++ Assets/Scripts/EnvironmentScroll.cs
@@ -8,6 +8,9 @@ public class EnvironmentScroll : MonoBehaviour
     // The scroll rate of the environmental object
     [SerializeField] private float scrollRate = 1f;
 
+    [SerializeField] private float spriteWidth;
+    private Vector3 startPosition;
+
     void Awake()
     {
         // Check if the BPM has been set
@@ -17,11 +20,23 @@ public class EnvironmentScroll : MonoBehaviour
             bpm = 160f;
         }
         else { bpm = songInfo.GetBPM(); }
+
+        if(spriteWidth == 0) {
+            spriteWidth = 1000;
+        }
+
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x - bpm / 4f / 2f * scrollRate * Time.deltaTime, transform.position.y, transform.position.z);
+        transform.Translate(Vector3.left * bpm / 4f / 2f * scrollRate * Time.deltaTime);
+
+        // If the element is off-screen, move it back on screen
+        if(transform.position.x < startPosition.x - (spriteWidth/100f * 2.5f))
+        {
+            transform.position = startPosition;
+        }
     }
 }
